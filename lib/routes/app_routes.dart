2d7d30a6ba6a1c71eb6cb00b6e543b1ea682e0e0
Pham Doan Hieu <hieubagiang@yahoo.com.vn>
2023-07-18 03:51:39 +0700

    update version to 0.3.2+3

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,5 @@
+class AliceRoutes {
+  static const String aliceCallsListPage = '/alice';
+  static const String aliceCallsDetailPage = '/aliceDetail';
+  static const String aliceCallsStatsPage = '/aliceStats';
+}
